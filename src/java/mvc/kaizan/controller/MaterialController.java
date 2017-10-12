635e0d834ff7a6f349735fed6d13bac273e20884
haqi <haqi@KaizanPC>
2017-10-13 05:45:48 +0700

    Java MVC Web ERP APP
    10/13/2017 5:45 AM

---
+++ src/java/mvc/kaizan/controller/MaterialController.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.kaizan.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+/**
+ *
+ * @author haqi
+ */
+public class MaterialController extends AbstractController {
+    
+    public MaterialController() {
+    }
+    
+    protected ModelAndView handleRequestInternal(
+            HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
---
+++ web/WEB-INF/jsp/index.jsp
@@ -0,0 +1,21 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>ERP Java MVC</title>
+    </head>
+
+    <body>
+        <p>
+            <ul>
+                <li>Customer</li>
+                <li>Material</li>
+                <li>Order</li>
+                <li>Supplier</li>
+            </ul>
+        </p>
+    </body>
+</html>
